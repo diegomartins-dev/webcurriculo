52dd77dfd2b79e90f11ed0b88312992c0f65b826
diegohyenna <dgmartins87@hotmail.com>
2019-11-24 19:39:13 -0300

    eh noix qui voa bruxao

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Eh noix qui voa bruxao!
         </a>
       </header>
     </div>
